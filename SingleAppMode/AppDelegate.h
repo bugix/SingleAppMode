a9580ad25e3623c3e42acf851149adc03a936e53
martin imobersteg <martin.imobersteg@gmail.com>
2014-10-28 12:51:15 +0100

    Initial Commit

---
+++ SingleAppMode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleAppMode
+//
+//  Created by Martin Imobersteg on 28.10.14.
+//  Copyright (c) 2014 Catnip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
